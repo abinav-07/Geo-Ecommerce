578b3017f8e6235640aba75ccc4fce917b860a74
abinav-07 <abighimire07@gmail.com>
2020-12-15 21:59:34 +0545

    Initial

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Layout } from 'antd';
+import { Content, Footer, Header } from 'antd/lib/layout/layout';
+
+const LoginPage=()=>{
+    return(
+       <div>
+           <Layout>
+               <Header>
+                    
+               </Header>
+               <Content>
+
+               </Content>
+               <Footer>
+
+               </Footer>
+           </Layout>
+       </div> 
+    )
+}
+export default LoginPage;
